7ed131319513843451a57bc83d6c3e409a6aa549
LuisPablo19 <111162493+LuisPablo19@users.noreply.github.com>
2023-06-05 18:56:16 -0600

    Add files via upload

--- evidenciafinaluf6_a00572040.py
+++ evidenciafinaluf6_a00572040.py
@@ -7,20 +7,11 @@ Original file is located at
     https://colab.research.google.com/drive/1qUoHakoFO_EC08c25OKG1UsCxkNe9z8G
 """
 
-import pandas as pd
 import streamlit as st
-
+import pandas as pd
+st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 df = pd.read_csv("https://drive.google.com/drive/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ")
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
-
-mapa = pd.DataFrame()
-mapa['Date'] = df['Incident Date']
-mapa['Day'] = df['Incident Day of Week']
-mapa['Police District'] = df['Police District']
-mapa['Neighborhood'] = df['Analysis Neighborhood']
-mapa['Incident Category'] = df['Incident Category']
-mapa['Incident Subcategory'] = df['Incident Subcategory']
-mapa['Resolution'] = df['Resolution']
-mapa['lat'] = df['Latitude']
-mapa['lon'] = df['Longitude']
+mapa=pd.DataFrame()
 mapa = mapa.dropna()
+st.map(mapa.astype(int))
